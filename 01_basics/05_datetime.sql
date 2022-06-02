3391b658446051a374b013b691dcd04bc3627c8a
akshaymalik1995 <akshaymalik191@gmail.com>
2022-06-02 16:27:47 +0530

    DATETIME

---
+++ 01_basics/05_datetime.sql
@@ -0,0 +1,21 @@
+-- CREATE TABLE temp_users(
+--     name VARCHAR(30),
+--     col1 DATE,
+--     col2 TIME, 
+--     col3 DATETIME
+-- )
+
+-- DESC temp_users
+
+-- INSERT INTO temp_users(name, col1, col2, col3)
+-- VALUES
+-- ('akshay', '1967-02-03', '10:23:22', '1967-02-03 10:23:22')
+
+-- INSERT INTO temp_users(name, col1, col2, col3)
+-- VALUES
+-- ('hitesh', '1967-02-03', '10:23:22', NOW())
+
+-- SELECT * FROM temp_users
+
+-- DROP TABLE temp_users
+
